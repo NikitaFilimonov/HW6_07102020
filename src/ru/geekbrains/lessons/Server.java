4d50dc75a2ac9a4ea5990f5ff6697b48abaedb12
NikitaFilimonov <69960509+NikitaFilimonov@users.noreply.github.com>
2020-10-09 14:54:49 +0300

    HW6

--- src/ru/geekbrains/lessons/Server.java
+++ src/ru/geekbrains/lessons/Server.java
@@ -4,49 +4,97 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
+import java.io.*;
 
 public class Server {
-    public static void main(String[] args) {
-        ServerSocket server = null;
-        Socket socket = null;
-        final int PORT = 8189;
-
-        try {
-            server = new ServerSocket(PORT);
-            System.out.println("Server started");
-            socket = server.accept();
-            System.out.println("Client connected");
-
-            Scanner in = new Scanner(socket.getInputStream());
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-
-
-            while (true) {
-                String str = in.nextLine();
-
-                if (str.equals("/end")) {
-                    break;
+    public static void main(String[] args) throws IOException {
+        SRV s = new SRV();
+        s.start();
+        s.catchClient();
+        new Thread() {
+            public void run() {
+                while (true) {
+                    String txt = null;
+                    try {
+                        txt = s.in.readLine();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    if (txt != null) {
+                        try {
+                            s.sendMessage(txt);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
                 }
-
-                System.out.println("Client: " + str);
-                out.println("echo: " + str);
             }
+        }.start();
 
+        new Thread() {
+            public void run() {
+                try {
+                    s.writeToConsole();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+    }
+}
+
+class SRV {
+    BufferedReader in = null;
+    PrintWriter out = null;
+    ServerSocket serverSocket = null;
+    Socket socket = null;
+    String input;
+    BufferedReader console = null;
+
+    void start() {
+        try {
+            serverSocket = new ServerSocket(8189);
         } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                socket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            System.out.println("Can't open port");
+            System.exit(1);
+        }
+        System.out.print("Server started. Waiting for a client...");
+    }
 
-            try {
-                server.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+    void catchClient() throws IOException {
+        try {
+            socket = serverSocket.accept();
+            System.out.println("Client connected");
+        } catch (IOException e) {
+            System.out.println("Can't accept");
+            System.exit(1);
+        }
+
+        in  = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(),true);
+
+        System.out.println("Wait for messages...");
+    }
+
+    void sendMessage(String msg) throws IOException {
+        if (msg.equalsIgnoreCase("exit")) close();
+        out.println("Server: " + msg);
+        System.out.println(msg);
+    }
+
+    void close() throws IOException {
+        out.close();
+        in.close();
+        socket.close();
+        serverSocket.close();
+    }
+
+    void writeToConsole() throws IOException {
+        while (true) {
+            console = new BufferedReader(new InputStreamReader(System.in));
+            String txt = console.readLine();
+            sendMessage(txt);
         }
     }
 }
